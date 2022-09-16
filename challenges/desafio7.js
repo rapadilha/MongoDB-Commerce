2319de7faad70936c9fcd3cfcf8affd3c822fbbd
Raphael <raphaellfpg@gmail.com>
2022-09-16 02:54:00 -0300

    Desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,14 @@
+db.produtos.find({
+  $and: [
+    {
+    tags: { $exists: false },
+    },
+    {
+      vendidos: { $ne: 50 },
+    },
+  ],
+}, {
+    _id: 0,
+    nome: 1,
+    vendidos: 1,
+  });
